a64e48faa15e466ce3e07e28262f45296d5229c9
Darkelf2024 <174353864+Darkelf2024@users.noreply.github.com>
2025-06-09 03:20:11 -0500

    Update Darkelf Stealth OSINT 768.py
    Removed Comment
    
    Signed-off-by: Darkelf2024 <174353864+Darkelf2024@users.noreply.github.com>

--- Darkelf Stealth OSINT 768.py
+++ Darkelf Stealth OSINT 768.py
@@ -128,7 +128,7 @@ import psutil
 from PIL import Image
 import piexif
 
-# Please run rotate_tls.py = Need to run it only one time in your system - Please refer to Github Repo
+
 class PhishingDetectorZeroTrace:
     """
     Zero-trace phishing detection for Darkelf:
