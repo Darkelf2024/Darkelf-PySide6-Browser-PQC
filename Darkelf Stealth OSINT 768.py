4217e1429e2df6ab1a5b0dc6d33e13f9573f090e
Darkelf2024 <174353864+Darkelf2024@users.noreply.github.com>
2025-06-11 05:11:54 -0500

    Update Darkelf Stealth OSINT 768.py
    Signed-off-by: Darkelf2024 <174353864+Darkelf2024@users.noreply.github.com>

--- Darkelf Stealth OSINT 768.py
+++ Darkelf Stealth OSINT 768.py
@@ -191,8 +191,7 @@ class DarkelfKernelMonitor(threading.Thread):
         keys = [
             "kern.osrevision",
             "kern.osversion",
-            "kern.bootargs",
-            "vm.swapusage"
+            "kern.bootargs"
         ]
         results = {}
         for key in keys:
