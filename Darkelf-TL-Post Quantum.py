239aafb3615272d68d427a8ba55ae7e1bd2d6ea2
Darkelf2024 <174353864+Darkelf2024@users.noreply.github.com>
2025-06-12 09:39:29 -0500

    Update Darkelf-TL-Post Quantum.py
    Kill Mode Confirmed Fixed in Kernel Monitor
    
    Signed-off-by: Darkelf2024 <174353864+Darkelf2024@users.noreply.github.com>

--- Darkelf-TL-Post Quantum.py
+++ Darkelf-TL-Post Quantum.py
@@ -79,6 +79,7 @@ import oqs
 import socks
 import warnings
 import mmap
+import signal
 import nacl.public
 from nacl.public import PrivateKey, PublicKey
 from nacl.exceptions import CryptoError
@@ -207,7 +208,7 @@ class DarkelfKernelMonitor(threading.Thread):
     def shutdown_darkelf(self):
         print("💣 [DarkelfKernelMonitor] Closing Darkelf app due to swap activation...")
         if self.parent_app:
-            self.parent_app.quit()
+            QTimersingleShot(0, self.parent_app.quit)
         else:
             os.kill(os.getpid(), signal.SIGTERM)
 
