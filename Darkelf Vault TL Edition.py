2cb05a6711e5cfd0dc2f24cdd1875e50579eeb96
Darkelf2024 <174353864+Darkelf2024@users.noreply.github.com>
2025-06-19 04:45:03 -0500

    Update Darkelf Vault TL Edition.py
    onion lite sites in
    
    Signed-off-by: Darkelf2024 <174353864+Darkelf2024@users.noreply.github.com>

--- Darkelf Vault TL Edition.py
+++ Darkelf Vault TL Edition.py
@@ -3792,7 +3792,7 @@ class Darkelf(QMainWindow):
             <div class="content">
                 <h1>Darkelf Browser</h1>
                 <p>Your privacy is our priority.</p>
-                <form id="searchForm" action="https://lite.duckduckgo.com/lite/" method="get">
+                <form id="searchForm" action="https://duckduckgogg42xjoc72x3sjasowoarfbgcmvfimaftt6twagswzczad.onion/lite/" method="get">
                     <input type="text" id="searchInput" name="q" placeholder="Search DuckDuckGo">
                     <button type="submit"><i class="bi bi-search"></i></button>
                 </form>
